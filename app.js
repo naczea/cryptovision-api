0a49ccc15ac60c24aa85329123a858ad55cb9ec8
naczea <nico.acz21@gmail.com>
2022-01-26 22:54:19 -0500

    api-rest

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const mysql = require('mysql');
-
 const bodyParser = require('body-parser');
+var arr = require('./data').arr;
 
 const PORT = process.env.PORT || 3050;
 
@@ -18,51 +17,30 @@ app.use((req, res, next) => {
 });
 
 
-// MySql
-const connection = mysql.createConnection({
-  host: 'bubpuaypz3zhlrtc3emy-mysql.services.clever-cloud.com',
-  user: 'u6kqskldskghamce',
-  password: 'anz222FsDh1YUYvjjspX',
-  database: 'bubpuaypz3zhlrtc3emy'
-});
-
 // Route
 app.get('/', (req, res) => {
-  res.send('Welcome to my ssAPI 2!');
+  res.send('Welcome to crypto');
 });
 
 // all customers
-app.get('/users', (req, res) => {
-  const sql = 'SELECT * FROM users';
-
-  connection.query(sql, (error, results) => {
-    if (error) throw error;
-    if (results.length > 0) {
-      res.json(results);
-    } else {
-      res.send('Not result');
-    }
-  });
+app.get('/crypto/get', (req, res) => {
+  res.json(arr);
 });
 
-app.post('/add', (req, res) => {
-  const sql = 'INSERT INTO users SET ?';
+app.post('/crypto/post', (req, res) => {
 
   const customerObj = {
-    username: req.body.username,
-    email: req.body.email
+    name: req.body.name,
+    current_price: req.body.current_price,
+    image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
   };
 
-  connection.query(sql, customerObj, error => {
-    if (error) throw error;
-    res.send('User created!');
-  });
-});
-
-// Check connect
-connection.connect(error => {
-  if (error) throw error;
-  console.log('Database server running!');
+  try {
+    arr.push(customerObj);
+    res.send('Coin created!');
+  }catch(er){
+    console.error(er);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ data.js
@@ -0,0 +1,29 @@
+var arr = [
+    {
+        name: "Bitcoin",
+        current_price: 35815,
+        image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+    },
+    {
+        name: "Ethereum",
+        current_price: 2391.52,
+        image: "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880"
+    },
+    {
+        name: "Tether",
+        current_price: 1.0,
+        image: "https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707"
+    },
+    {
+        name: "Binance Coin",
+        current_price: 367.44,
+        image: "https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png?1547034615"
+    },
+    {
+        name: "USD Coin",
+        current_price: 10.25,
+        image: "https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png?1547042389"
+    }
+];
+
+exports.arr = arr;
